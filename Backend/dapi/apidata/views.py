bf2229dcbacfd90c7e9a8b11b474f7c7e90ee318
lulucatty <lulucatty262@gmail.com>
2022-08-09 08:54:27 -0700

    save and load python file

--- Backend/dapi/apidata/views.py
+++ Backend/dapi/apidata/views.py
@@ -1,3 +1,4 @@
+from django.template import RequestContext
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet 
 from .models import testresult
@@ -16,17 +17,25 @@ def mydata(request):
         firstname=form.cleaned_data['firstname']
         lastname=form.cleaned_data['lastname']
         age=form.cleaned_data['age']
-        phyicalhealth=form.cleaned_data['phyicalhealth']
         insulin=form.cleaned_data['insulin']
         BMI=form.cleaned_data['BMI']
         Bloodpressure=form.cleaned_data['Bloodpressure']
-        Glucose=form.cleaned_data['glucose']
-        eyedamage=form.cleaned_data['eyedamage']
+        # Glucose=form.cleaned_data['glucose']
+        # eyedamage=form.cleaned_data['eyedamage']
         retinopathy=form.cleaned_data['retinopathy']
+
+        with open('trained_model', 'rb') as trained_model:
+            return
+        diabetic= pickle.load(trained_model)
+
+    
+ 
+    
 
        
         
         
     form=testform()
     return render(request, 'testing.html', {'form':form})
+    return render_to_response('fileupload/upload.html', {'testing': c['UploadFiletesting']},  RequestContext(request))
 
