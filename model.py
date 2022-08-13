0b78544551eeb27b9c2f10682be8039dbc105b57
lulucatty <lulucatty262@gmail.com>
2022-08-13 12:31:17 -0700

    load and save files

--- model.py
+++ model.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import sklearn
 import shap
+import io
+import pickle
 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
@@ -32,13 +34,16 @@ print (df)
 x= df[['Glucose','retinopathy','BMI','Blood pressure','phyical health','Age']]# Features
 y= df['Output'] # Labels
 clf=RandomForestClassifier(n_estimators=100)
+with open('trained_model', 'wb') as trained_model:
+    pickle.dump(df, trained_model)
+
 # Split dataset into traning set and test set
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3) # 70% training and 30% test
 clf.fit(x_train,y_train)
 y_pred=clf.predict(x_test)
 print("Accuracy:",metrics.accuracy_score(y_test,y_pred))
 
-explainer = shap.TreeExplainer(clf)
-shap_values = explainer.shap_values(x_test)
-  #To plot feature importance as the horizontal bar plot we need to use summary_plot method:
-shap.summary_plot(shap_values, x_test, plot_type="bar")
+# explainer = shap.TreeExplainer(clf)
+# shap_values = explainer.shap_values(x_test)
+#   #To plot feature importance as the horizontal bar plot we need to use summary_plot method:
+# shap.summary_plot(shap_values, x_test, plot_type="bar")
