c49e1ff5bbc16e6cbd3208c0d8ca3ed8d195dc37
lulucatty <lulucatty262@gmail.com>
2022-08-18 13:38:19 -0700

    loading python files

--- Backend/dapi/apidata/views.py
+++ Backend/dapi/apidata/views.py
@@ -8,6 +8,9 @@ from .serializers import testresultSerializers
 import pickle
 from django.views.decorators.csrf import csrf_exempt
 
+with open('trained_model', 'rb') as trained_model:
+    clf= pickle.load(trained_model)
+
 class Adressview (ModelViewSet):
     serializer_class = testresultSerializers
     queryset = testresult.objects.all()
@@ -17,20 +20,28 @@ def mydata(request):
     print("Hello")
     print(request)
     if request.method=='POST':
-        form=testform(request.POST)
+      
+        firstname=request.POST.get['firstname']
+        lastname=request.POST.get['lastname']
+        age=request.POST.get['age']
+        insulin=request.POST.get['insulin']
+        BMI=request.POST.get['BMI']
+        Bloodpressure=request.POST.get['Bloodpressure']
+        retinopathy=request.POST.get['retinopathy'] 
+        # form=testform(request.POST)
         #validation
-        firstname=form.cleaned_data['firstname']
-        lastname=form.cleaned_data['lastname']
-        age=form.cleaned_data['age']
-        insulin=form.cleaned_data['insulin']
-        BMI=form.cleaned_data['BMI']
-        Bloodpressure=form.cleaned_data['Bloodpressure']
-        # Glucose=form.cleaned_data['glucose']
-        # eyedamage=form.cleaned_data['eyedamage']
-        retinopathy=form.cleaned_data['retinopathy'] 
-
-        with open('trained_model', 'rb') as trained_model:
-            clf= pickle.load(trained_model)
+        # firstname=form.cleaned_data['firstname']
+        # lastname=form.cleaned_data['lastname']
+        # age=form.cleaned_data['age']
+        # insulin=form.cleaned_data['insulin']
+        # BMI=form.cleaned_data['BMI']
+        # Bloodpressure=form.cleaned_data['Bloodpressure']
+        # # Glucose=form.cleaned_data['glucose']
+        # # eyedamage=form.cleaned_data['eyedamage']
+        # retinopathy=form.cleaned_data['retinopathy'] 
+    sample=[[firstname,lastname,age,insulin,BMI,Bloodpressure,retinopathy]]
+    result=clf.predict(sample)
+    print(result)
 
     
  
@@ -39,7 +50,7 @@ def mydata(request):
        
         
         
-    form=testform()
-    return render(request, 'testing.html', {'form':form})
-    return render_to_response('fileupload/upload.html', {'testing': c['UploadFiletesting']},  RequestContext(request))
+    # form=testform()
+    # return render(request, 'testing.html', {'form':form})
+    # return render_to_response('fileupload/upload.html', {'testing': c['UploadFiletesting']},  RequestContext(request))
 
